55add22c0130563ab2f1e73d5779ed82cda5839e
Matt Auger <sc20mra@leeds.ac.uk>
2024-05-20 17:07:58 +0100

    Basic Implementation of Incoming / Outgoing Links

--- crawler.py
+++ crawler.py
@@ -34,7 +34,7 @@ def tokenize(content):
     # [^\p{L} ] regexp for anything which isn't a letter (includes accented
     # chars like in André)
     all_tokens = wordpunct_tokenize(content)
-
+    num_tokens = len(all_tokens)
     index = {}
     # punctuation_marks = {",", ".", "'", "-", ":", ";", "(", ")", "”", "“"}
     # all_stopwords = set(stopwords.words("english")).union(punctuation_marks)
@@ -51,11 +51,11 @@ def tokenize(content):
         else:
             # Create a new index entry for this token
             index[token] = [position]
-    return index
+    return index, num_tokens
 
 
 def update_index(index, tokens, url):
-    print(f"Updating index with page contents.")
+    print(f"Indexing page content.")
     output_index = index
     for token, token_entries in tokens.items():
         if token in output_index:
@@ -69,7 +69,7 @@ def crawl():
     nltk.download("stopwords")
     nltk.download("punkt")
     urls = [ROOT_URL]
-    data = {ROOT_URL: {"visited": False, "content": ""}}
+    pages = {ROOT_URL: {"visited": False, "content": ""}}
     index = {}
     next_request_time = datetime.now()
     while len(urls) != 0:
@@ -87,15 +87,16 @@ def crawl():
         for e in elements:
             content += e.get_text().strip() + "\n"
 
-        tokens = tokenize(content)
+        tokens, tokens_count = tokenize(content)
 
-        data[current_url]["visited"] = True
-        data[current_url]["content"] = tokens
+        pages[current_url]["visited"] = True
+        pages[current_url]["tokens"] = tokens_count
         index = update_index(index, tokens, current_url)
 
         # Find all link tags
         links = soup.select("a[href]")
         new_urls = []
+        outgoing_urls = set({})
 
         for l in links:
             url = l["href"]
@@ -104,10 +105,18 @@ def crawl():
                 continue
             # Construct full url
             full_url = ROOT_URL + url
-            if full_url not in data:
+            outgoing_urls.add(full_url)
+            if full_url not in pages:
                 urls.append(full_url)
                 new_urls.append(full_url)
-                data[full_url] = {"visited": False}
+                pages[full_url] = {"visited": False, "incoming": [current_url]}
+            else:
+                existing_incoming = pages[full_url]["incoming"]
+                new_incoming = set(existing_incoming)
+                existing_incoming.add(current_url)
+                print(f"Updating incoming links for {full_url}")
+                pages[full_url]["incoming"] = list(new_incoming)
+        pages[current_url]["outgoing"] = list(outgoing_urls)
         # Log a message if new links were found on a page
         if len(new_urls) > 0:
             print(f"{len(new_urls)} new pages discovered:")
@@ -121,6 +130,10 @@ def crawl():
         # Write the inverse index to local storage
         with open("inverse_index.json", "w") as fp:
             json.dump(index, fp)
+
+        # Write pages to local storage
+        with open("pages.json", "w") as fp:
+            json.dump(pages, fp)
 
         # Find the time remaining before another request can be made
         sleep_time = (abs(datetime.now() - next_request_time)).total_seconds()
